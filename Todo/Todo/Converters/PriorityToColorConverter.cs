3cf069d224a177fd06d68a222c2af9a4b2a3176d
Clarks1223 <pablo.uchuari@epn.edu.ec>
2024-01-10 23:30:54 -0500

    Add project files.

---
+++ Todo/Todo/Converters/PriorityToColorConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Todo.Converters
+{
+    public class PriorityToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string priority = value as string;
+
+            switch (priority)
+            {
+                case "Alta":
+                    return Color.Red;
+                case "Media":
+                    return Color.Yellow;
+                case "Baja":
+                    return Color.Green;
+                default:
+                    return Color.Black; // Puedes cambiar esto a otro color por defecto si es necesario
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
